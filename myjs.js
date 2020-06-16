a779cd1d17cf7b66a1d055483670225ae6220f13
Jackson Foster <jacksonf1999@gmail.com>
2020-06-16 09:40:22 -0500

    Added the changes for the two button layout to the main files.

--- myjs.js
+++ myjs.js
@@ -1,53 +1,58 @@
-var theTime = document.getElementById("clock");
-var button = document.getElementById("action-btn"); 
 var myInterval = -1;
+var theTime = document.getElementById("clock");
 
-button.addEventListener("click", function(event) {
-if (myInterval == -1) {
-  button.innerHTML="Pause";
-  myInterval = setInterval(function () {
-  
-  var d = new Date();
-  /* Define variables that get the current time and date. */
-  var hours = d.getHours();
-  var mins = d.getMinutes();
-  var secs = d.getSeconds();
+function startBtn () {
+  if (myInterval == -1) {
+    myInterval = setInterval(function () {
+      var d = new Date();
+      /* Define variables that get the current time and date. */
+      var hours = d.getHours();
+      var mins = d.getMinutes();
+      var secs = d.getSeconds();
 
-  if (hours > 11) {
-    document.getElementById("am-pm").innerHTML = " P.M.";
+      if (hours > 12) {
+        document.getElementById("am-pm").innerHTML = "PM"
+        hours = hours - 12;
+      }
+
+
+      hours = timeData(hours);
+      mins = getTime(mins);
+      secs = getTime(secs); 
+
+      function getTime(i) {
+      if (i < 10) {
+        i = "0" + i;
+      }
+      return i;
+      }
+
+      function timeData(n) {
+      if (n == 0) {
+        n = "12";
+      }
+      return n;
+      } 
+      theTime.innerHTML = hours + ":" + mins + ":" + secs;
+    }, 1000);
   }
-  if (hours > 12) {
-    hours = hours - 12;
-  }
+}
 
-  hours = timeData(hours);
-  mins = getTime(mins);
-  secs = getTime(secs);
-
-  theTime.innerHTML = hours + ":" + mins + ":" + secs; 
-
-  function getTime(i) {
-    if (i < 10) {
-      i = "0" + i;
-    }
-    return i;
-  }
-
-  function timeData(n) {
-    if (n == 0) {
-      n = "12";
-    }
-    return n;
-  } 
-  
-  }, 1000); 
-  
-  } else {
+function stopBtn () {
+  if (myInterval != -1) {
     clearInterval(myInterval);
     myInterval = -1;
-    button.innerHTML ="Start";
   }
-})
+}
+
+startBtn();
+
+document.getElementById("start-btn").onclick = function() {
+  startBtn();
+}
+document.getElementById("stop-btn").onclick = function () {
+  stopBtn();
+}
 
 
   
